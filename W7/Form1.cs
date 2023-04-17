9aada40556ad6c15d200813b5da841256182d007
Steivann <127372330+Steivann@users.noreply.github.com>
2023-04-17 08:17:58 +0700

    Add files via upload

---
+++ W7/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label judul = new Label();
+            judul.Text = "Cinema";
+            judul.Size = new Size(200, 100);
+            judul.Location = new Point(333, 50);
+            Controls.Add(judul);
+
+            PictureBox Filmironman = new PictureBox();
+            Filmironman.Image = Properties.Resources.foto1;
+            Filmironman.Location = new Point(12, 100);
+            Filmironman.Name = "Filmironman";
+            Filmironman.Size = new Size(133, 175);
+            Filmironman.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmironman);
+
+            Button ironman = new Button();
+            ironman.Size = new Size(80, 20);
+            ironman.Location = new Point(39, 285);
+            ironman.Text = "Watch";
+            Controls.Add(ironman);
+            ironman.Click += Ironman_Click;
+
+            PictureBox Filmit = new PictureBox();
+            Filmit.Image = Properties.Resources.foto2;
+            Filmit.Location = new Point(200, 100);
+            Filmit.Name = "Filmit";
+            Filmit.Size = new Size(133, 175);
+            Filmit.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmit);
+
+            Button it = new Button();
+            it.Size = new Size(80, 20);
+            it.Location = new Point(227, 285);
+            it.Text = "Watch";
+            Controls.Add(it);
+            it.Click += It_Click;
+
+            PictureBox Filmjoker = new PictureBox();
+            Filmjoker.Image = Properties.Resources.foto4;
+            Filmjoker.Location = new Point(388, 100);
+            Filmjoker.Name = "Filmjoker";
+            Filmjoker.Size = new Size(133, 175);
+            Filmjoker.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmjoker);
+
+            Button joker = new Button();
+            joker.Size = new Size(80, 20);
+            joker.Location = new Point(415, 285);
+            joker.Text = "Watch";
+            Controls.Add(joker);
+            joker.Click += Joker_Click;
+
+            PictureBox Filmbatman = new PictureBox();
+            Filmbatman.Image = Properties.Resources.foto5;
+            Filmbatman.Location = new Point(576, 100);
+            Filmbatman.Name = "Filmbatman";
+            Filmbatman.Size = new Size(133, 175);
+            Filmbatman.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmbatman);
+
+            Button batman = new Button();
+            batman.Size = new Size(80, 20);
+            batman.Location = new Point(603, 285);
+            batman.Text = "Watch";
+            Controls.Add(batman);
+            batman.Click += Batman_Click;
+
+            PictureBox Filmdeadpool = new PictureBox();
+            Filmdeadpool.Image = Properties.Resources.foto8;
+            Filmdeadpool.Location = new Point(12, 340);
+            Filmdeadpool.Name = "Filmdeadpool";
+            Filmdeadpool.Size = new Size(133, 175);
+            Filmdeadpool.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmdeadpool);
+
+            Button deadpool = new Button();
+            deadpool.Size = new Size(80, 20);
+            deadpool.Location = new Point(39, 525);
+            deadpool.Text = "Watch";
+            Controls.Add(deadpool);
+            deadpool.Click += Deadpool_Click;
+
+            PictureBox Filmlightyear = new PictureBox();
+            Filmlightyear.Image = Properties.Resources.foto7;
+            Filmlightyear.Location = new Point(200, 340);
+            Filmlightyear.Name = "Filmlightyear";
+            Filmlightyear.Size = new Size(133, 175);
+            Filmlightyear.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmlightyear);
+
+            Button lightyear = new Button();
+            lightyear.Size = new Size(80, 20);
+            lightyear.Location = new Point(227, 525);
+            lightyear.Text = "Watch";
+            Controls.Add(lightyear);
+            lightyear.Click += Lightyear_Click;
+
+            PictureBox Filmjurasic = new PictureBox();
+            Filmjurasic.Image = Properties.Resources.foto6;
+            Filmjurasic.Location = new Point(388, 340);
+            Filmjurasic.Name = "Filmjurasic";
+            Filmjurasic.Size = new Size(133, 175);
+            Filmjurasic.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmjurasic);
+
+            Button jurasic = new Button();
+            jurasic.Size = new Size(80, 20);
+            jurasic.Location = new Point(415, 525);
+            jurasic.Text = "Watch";
+            Controls.Add(jurasic);
+            jurasic.Click += Jurasic_Click;
+
+            PictureBox Filmscream = new PictureBox();
+            Filmscream.Image = Properties.Resources.foto9;
+            Filmscream.Location = new Point(576, 340);
+            Filmscream.Name = "Filmscream";
+            Filmscream.Size = new Size(133, 175);
+            Filmscream.SizeMode = PictureBoxSizeMode.StretchImage;
+            Controls.Add(Filmscream);
+
+            Button scream = new Button();
+            scream.Size = new Size(80, 20);
+            scream.Location = new Point(603, 525);
+            scream.Text = "Watch";
+            Controls.Add(scream);
+            scream.Click += Scream_Click;
+
+        }
+
+        private void Scream_Click(object sender, EventArgs e)
+        {
+            Formfilmscream movie = new Formfilmscream();
+            movie.Show();
+        }
+
+        private void Jurasic_Click(object sender, EventArgs e)
+        {
+            Formfilmjurasic movie = new Formfilmjurasic();
+            movie.Show();
+        }
+
+        private void Lightyear_Click(object sender, EventArgs e)
+        {
+            Formfilmlightyear movie = new Formfilmlightyear();
+            movie.Show();
+        }
+
+        private void Deadpool_Click(object sender, EventArgs e)
+        {
+            Formfilmdeadpool movie = new Formfilmdeadpool();
+            movie.Show();
+        }
+
+        private void Batman_Click(object sender, EventArgs e)
+        {
+            Formfilmbatman movie = new Formfilmbatman();
+            movie.Show();
+        }
+
+        private void Joker_Click(object sender, EventArgs e)
+        {
+            Formfilmjoker movie = new Formfilmjoker();
+            movie.Show();
+        }
+
+        private void It_Click(object sender, EventArgs e)
+        {
+            formfilmit movie = new formfilmit();
+            movie.Show();
+        }
+
+        private void Ironman_Click(object sender, EventArgs e)
+        {
+            FormIronman movie = new FormIronman();
+            movie.Show();
+        }
+    }
+}
---
+++ W7/Formfilmjurasic.cs
@@ -0,0 +1,405 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W7
+{
+    public partial class Formfilmjurasic : Form
+    {
+        List<Button> buttonlist1 = new List<Button>();
+        List<Button> buttonlist2 = new List<Button>();
+        List<Button> buttonlist3 = new List<Button>();
+        Panel panell = new Panel();
+        Random gatau = new Random();
+
+        int a = 0;
+        int b = 0;
+        int bantu = 0;
+        public Formfilmjurasic()
+        {
+            InitializeComponent();
+        }
+
+        private void Formfilmjurasic_Load(object sender, EventArgs e)
+        {
+            Button jadwal1 = new Button();
+            jadwal1.Size = new Size(70, 70);
+            jadwal1.Location = new Point(20, 50);
+            jadwal1.Text = "08.00";
+            Controls.Add(jadwal1);
+            jadwal1.Click += Jadwal1_Click;
+
+            Button jadwal2 = new Button();
+            jadwal2.Size = new Size(70, 70);
+            jadwal2.Location = new Point(20, 125);
+            jadwal2.Text = "10.00";
+            Controls.Add(jadwal2);
+            jadwal2.Click += Jadwal2_Click;
+
+            Button jadwal3 = new Button();
+            jadwal3.Size = new Size(70, 70);
+            jadwal3.Location = new Point(20, 200);
+            jadwal3.Text = "12.00";
+            Controls.Add(jadwal3);
+            jadwal3.Click += Jadwal3_Click;
+
+            Button back = new Button();
+            back.Size = new Size(70, 30);
+            back.Location = new Point(30, 300);
+            back.Text = "Back";
+            Controls.Add(back);
+            back.Click += Back_Click;
+
+            panell.Location = new Point(240, 25);
+            panell.Size = new Size(280, 300);
+            panell.BackColor = Color.White;
+            panell.TabIndex = 0;
+            Controls.Add(panell);
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Okei2_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            foreach (Button a in buttonlist2)
+            {
+                if (a.BackColor == Color.YellowGreen)
+                {
+                    a.BackColor = Color.Red;
+                    a.Enabled = false;
+                    pilihan.Text = "Seat Choose : ";
+                }
+            }
+        }
+
+        private void Okei3_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            foreach (Button a in buttonlist3)
+            {
+                if (a.BackColor == Color.YellowGreen)
+                {
+                    a.BackColor = Color.Red;
+                    a.Enabled = false;
+                    pilihan.Text = "Seat Choose : ";
+                }
+            }
+        }
+
+        private void Okei1_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            foreach (Button a in buttonlist1)
+            {
+                if (a.BackColor == Color.YellowGreen)
+                {
+                    a.BackColor = Color.Red;
+                    a.Enabled = false;
+                    pilihan.Text = "Seat Choose : ";
+                }
+            }
+        }
+        private void Okei_Click(object sender, EventArgs e)
+        {
+
+        }
+        int cek = 0;
+        private void Jadwal3_Click(object sender, EventArgs e)
+        {
+
+
+            panell.Controls.Clear();
+
+            Button reset3 = new Button();
+            reset3.Size = new Size(80, 20);
+            reset3.Location = new Point(5, 275);
+            reset3.Text = "Reset";
+            panell.Controls.Add(reset3);
+            reset3.Click += Reset3_Click;
+            Button okei3 = new Button();
+            okei3.Size = new Size(80, 20);
+            okei3.Location = new Point(190, 275);
+            okei3.Text = "Reserve";
+            panell.Controls.Add(okei3);
+            okei3.Click += Okei3_Click;
+
+            a = 0;
+            b = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Button kursi3 = new Button();
+                    kursi3.Size = new Size(20, 20);
+                    kursi3.Location = new Point(10 + a, 10 + b);
+                    kursi3.BackColor = Color.Black;
+                    kursi3.Tag = "(" + j.ToString() + "," + i.ToString() + ") ";
+                    buttonlist3.Add(kursi3);
+                    kursi3.Click += Kursi3_Click;
+                    bantu++;
+                    a += 20;
+                }
+                a = 0;
+                b += 20;
+            }
+
+            if (cek == 0)
+            {
+                for (int a = 0; a < 70; a++)
+                {
+                    int acak = gatau.Next(0, 100);
+                    buttonlist3[acak].BackColor = Color.Red;
+                    buttonlist3[acak].Enabled = false;
+                }
+            }
+            cek++;
+            foreach (Button buat in buttonlist3)
+            {
+                panell.Controls.Add(buat);
+            }
+        }
+
+        private void Reset3_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttonlist3)
+            {
+                if (button.BackColor == Color.Red)
+                {
+                    button.BackColor = Color.Black;
+                    button.Enabled = true;
+                }
+
+            }
+        }
+
+        private void Jadwal2_Click(object sender, EventArgs e)
+        {
+
+
+            panell.Controls.Clear();
+            Button reset2 = new Button();
+            reset2.Size = new Size(80, 20);
+            reset2.Location = new Point(5, 275);
+            reset2.Text = "Reset";
+            panell.Controls.Add(reset2);
+            reset2.Click += Reset2_Click;
+
+
+            Button okei2 = new Button();
+            okei2.Size = new Size(80, 20);
+            okei2.Location = new Point(190, 275);
+            okei2.Text = "Reserve";
+            panell.Controls.Add(okei2);
+            okei2.Click += Okei2_Click;
+
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+
+
+            a = 0;
+            b = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button kursi2 = new Button();
+                    kursi2.Size = new Size(20, 20);
+                    kursi2.Location = new Point(10 + a, 10 + b);
+                    kursi2.BackColor = Color.Black;
+                    kursi2.Tag = "(" + j.ToString() + "," + i.ToString() + ") ";
+                    buttonlist2.Add(kursi2);
+                    kursi2.Click += Kursi2_Click;
+                    bantu++;
+                    a += 20;
+                }
+                a = 0;
+                b += 20;
+            }
+            for (int a = 0; a < 70; a++)
+            {
+                int acak = gatau.Next(0, 100);
+                buttonlist2[acak].BackColor = Color.Red;
+                buttonlist2[acak].Enabled = false;
+            }
+            foreach (Button buat in buttonlist2)
+            {
+                panell.Controls.Add(buat);
+            }
+        }
+
+        private void Reset2_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttonlist2)
+            {
+                if (button.BackColor == Color.Red)
+                {
+                    button.BackColor = Color.Black;
+                    button.Enabled = true;
+                }
+
+            }
+        }
+
+        private void Jadwal1_Click(object sender, EventArgs e)
+        {
+
+
+            panell.Controls.Clear();
+            Button okei1 = new Button();
+            okei1.Size = new Size(80, 20);
+            okei1.Location = new Point(190, 275);
+            okei1.Text = "Reserve";
+            panell.Controls.Add(okei1);
+            okei1.Click += Okei1_Click;
+
+            Button reset1 = new Button();
+            reset1.Size = new Size(80, 20);
+            reset1.Location = new Point(5, 275);
+            reset1.Text = "Reset";
+            panell.Controls.Add(reset1);
+            reset1.Click += Reset1_Click;
+
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            a = 0;
+            b = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Button kursi1 = new Button();
+                    kursi1.Size = new Size(20, 20);
+                    kursi1.Location = new Point(10 + a, 10 + b);
+                    kursi1.BackColor = Color.Black;
+                    kursi1.Tag = "(" + j.ToString() + "," + i.ToString() + ") ";
+                    buttonlist1.Add(kursi1);
+                    kursi1.Click += Kursi1_Click;
+                    bantu++;
+                    a += 20;
+                }
+                a = 0;
+                b += 20;
+            }
+            for (int a = 0; a < 70; a++)
+            {
+                int acak = gatau.Next(0, 100);
+                buttonlist1[acak].BackColor = Color.Red;
+                buttonlist1[acak].Enabled = false;
+            }
+            foreach (Button buat in buttonlist1)
+            {
+                panell.Controls.Add(buat);
+            }
+
+        }
+
+        private void Reset1_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in buttonlist1)
+            {
+                if (button.BackColor == Color.Red)
+                {
+                    button.BackColor = Color.Black;
+                    button.Enabled = true;
+                }
+
+            }
+        }
+        private void Kursi3_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            Button sold = sender as Button;
+            foreach (Button duduk in buttonlist3)
+            {
+                panell.Controls.Add(duduk);
+            }
+            if (sold.BackColor == Color.Black)
+            {
+                sold.BackColor = Color.YellowGreen;
+                pilihan.Text += sold.Tag.ToString();
+            }
+        }
+
+
+
+        private void Kursi1_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            Button sold = sender as Button;
+            foreach (Button duduk in buttonlist3)
+            {
+                panell.Controls.Add(duduk);
+            }
+            if (sold.BackColor == Color.Black)
+            {
+                sold.BackColor = Color.YellowGreen;
+                pilihan.Text += sold.Tag.ToString();
+            }
+        }
+
+        private void Kursi2_Click(object sender, EventArgs e)
+        {
+            Label pilihan = new Label();
+            pilihan.Location = new Point(5, 240);
+            pilihan.Size = new Size(300, 300);
+            pilihan.Text = "Seat Choose : ";
+            panell.Controls.Add(pilihan);
+
+            Button sold = sender as Button;
+            foreach (Button duduk in buttonlist3)
+            {
+                panell.Controls.Add(duduk);
+            }
+            if (sold.BackColor == Color.Black)
+            {
+                sold.BackColor = Color.YellowGreen;
+                pilihan.Text += sold.Tag.ToString();
+            }
+        }
+    }
+}
